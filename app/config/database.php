24fac7329d842212cfcc4ebcea7bd86e02906616
Muhammad Yafi <yafithekid212@gmail.com>
2015-02-10 23:30:21 +0700

    change db password

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'taman_laravel',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'akuganteng',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
